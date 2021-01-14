94a7d9d78de4baad40bd4b67149897dc190e3430
Marko Ivancevic <ivancevic.1974@gmail.com>
2021-01-14 10:33:22 +0100

    Add Symfony Doctrine course

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,45 +2,98 @@
 
 namespace App\Controller;
 
+use App\Service\MarkdownHelper;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Entity\Question;
+use Doctrine\ORM\EntityManagerInterface;
 
 class QuestionController extends AbstractController
 {
+    private $logger;
+    private $isDebug;
+
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
+
+
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(Environment $twigEnvironment)
+    public function homepage()
     {
-        /*
-        // fun example of using the Twig service directly!
-        $html = $twigEnvironment->render('question/homepage.html.twig');
-
-        return new Response($html);
-        */
-
         return $this->render('question/homepage.html.twig');
     }
 
+    
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager) {
+        
+        $question = new Question();
+        
+        $question->setName('Mising pants')
+            ->setSlug('missing-pants'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?                
+EOF
+                );
+        
+            if (rand(1, 10) > 2) {
+                $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+            }
+            
+            $entityManager->persist($question);
+            $entityManager->flush();
+            
+        return new Response(sprintf(
+            'New question is id #%d, slug %s',
+            $question->getId(),
+            $question->getSlug(),
+            ));
+    }
+    
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
+        if ($this->isDebug) {
+            $this->logger->info('We are in debug mode!');
+        }
+        
+        $repository = $entityManager->getRepository(Question::class);
+        /** @var Question|null $question */
+        $question = $repository->findOneBy(['slug' => $slug]);
+        if (!$question) {
+            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
+        }
+       
+        
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
         
-        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        
+
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $questionText,
+            'question' => $question,
             'answers' => $answers,
         ]);
     }
+    
 }
